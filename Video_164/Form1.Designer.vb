0b829c8fa5ef88123e305d7aec42ce7a33befac1
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-07 20:35:50 -0500

    Finished

--- Video_164/Form1.Designer.vb
+++ Video_164/Form1.Designer.vb
@@ -22,17 +22,48 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnDraw = New System.Windows.Forms.Button()
+        Me.btnAnimate = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
+        '
+        'btnDraw
+        '
+        Me.btnDraw.Location = New System.Drawing.Point(12, 261)
+        Me.btnDraw.Name = "btnDraw"
+        Me.btnDraw.Size = New System.Drawing.Size(190, 23)
+        Me.btnDraw.TabIndex = 0
+        Me.btnDraw.Text = "Draw Rectangle"
+        Me.btnDraw.UseVisualStyleBackColor = True
+        '
+        'btnAnimate
+        '
+        Me.btnAnimate.Location = New System.Drawing.Point(208, 261)
+        Me.btnAnimate.Name = "btnAnimate"
+        Me.btnAnimate.Size = New System.Drawing.Size(190, 23)
+        Me.btnAnimate.TabIndex = 1
+        Me.btnAnimate.Text = "Animate Rectangle"
+        Me.btnAnimate.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 20
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(405, 296)
+        Me.Controls.Add(Me.btnAnimate)
+        Me.Controls.Add(Me.btnDraw)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents btnDraw As Button
+    Friend WithEvents btnAnimate As Button
+    Friend WithEvents Timer1 As Timer
 End Class
--- Video_164/Form1.vb
+++ Video_164/Form1.vb
@@ -1,3 +1,41 @@
 ﻿Public Class Form1
+    Private g As Graphics
+    Private xPos As Integer
+    Private yPos As Integer
+    Private polyPoints() As Point = {New Point(10, 10), New Point(30, 50), New Point(40, 100), New Point(40, 50)}
 
+    Private Sub btnDraw_Click(sender As Object, e As EventArgs) Handles btnDraw.Click
+        g.FillRectangle(Brushes.Black, 30, 30, 50, 40)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        g = Me.CreateGraphics
+    End Sub
+
+    Private Sub Form1_Click(sender As Object, e As MouseEventArgs) Handles MyBase.Click
+        xPos = e.X
+        yPos = e.Y
+        DrawNewRectangle(e.X, e.Y)
+    End Sub
+
+    Private Sub DrawNewRectangle(ByVal xPos As Integer, ByVal yPos As Integer)
+        Me.Refresh()
+        g.FillRectangle(Brushes.CadetBlue, xPos, yPos, 60, 60)
+    End Sub
+
+    Private Sub btnAnimate_Click(sender As Object, e As EventArgs) Handles btnAnimate.Click
+        'Timer1.Start()
+        'g.DrawLine(Pens.Black, New Point(10, 15), New Point(100, 90))
+        g.FillPolygon(Brushes.Black, polyPoints)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If xPos > Me.Size.Width Then
+            xPos = 0
+        ElseIf xPos < 0 Then
+            xPos = Me.Size.Width
+        End If
+        xPos += 3
+        DrawNewRectangle(xPos, yPos)
+    End Sub
 End Class
